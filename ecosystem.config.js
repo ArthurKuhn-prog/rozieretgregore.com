d4094d155e49ec4e80a2a8376ca07659b66e4319
Arthur Kuhn <a.kuhn@kuhnhestale.fr>
2022-03-25 18:26:38 +0000

    modified:   ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,14 +9,12 @@ module.exports = {
     deploy : {
       // "production" is the environment name
       production : {
-          key: '~/.ssh/deploy_keys/id_rsa',
+          key: '~/.ssh/id_ed25519',
         user: 'akuhn',
         host: ['217.160.249.136'],
         ref: 'origin/master',
         repo: 'git@ArthurKuhn-prog/rozieretgregore.com.git',
         ssh_options: [
-            "StrictHostKeyChecking=no",
-            "PasswordAuthentication=no"
         ],
         path: '/var/www/rozieretgregore.com',
       }
