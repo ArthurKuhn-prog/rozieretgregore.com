210d5e47120b583ed599228f915dd549fdebf594
Arthur Kuhn <a.kuhn@kuhnhestale.fr>
2022-03-25 17:53:01 +0000

    modified:   ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,7 +14,10 @@ module.exports = {
         host: ['217.160.249.136'],
         ref: 'origin/master',
         repo: 'git@ArthurKuhn-prog/rozieretgregore.com.git',
-        ssh_options: [],
+        ssh_options: [
+            "StrictHostKeyChecking=no",
+            "PasswordAuthentication=no"
+        ],
         path: '/var/www/rozieretgregore.com',
       }
     }
